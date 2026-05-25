aeb1c93b2f77686d3b61d73c131a3dff3dff37e8
Shivam <shivashivam175@gmail.com>
2026-05-25 13:51:14 +0530

    Connected frontend to live backend

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -37,16 +37,16 @@ export const ContactSection = () => {
     setIsSubmitting(true);
 
     try {
-      const response = await fetch(
-        "http://localhost:5000/api/contact",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(formData),
-        }
-      );
+  const response = await fetch(
+    "https://portfolio-backend-a0n7.onrender.com/api/contact",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    }
+  );
 
       const data = await response.json();
 
